1666a94da72189c2c106508407c85b2a95fc43c3
t.krivonogova <t.krivonogova@g.nsu.ru>
2022-11-25 22:56:52 +0700

    added cheking cmd arguments, try to move it to separate class

--- soundProcessor/main.cpp
+++ soundProcessor/main.cpp
@@ -26,18 +26,98 @@ size_t getNumberOfThreads(std::vector<std::string> command) {
 	return count;
 }
 
-int main() {
+std::string getCmdArgument( char** argv, size_t indexArgument) {
+	std::string arg = argv[indexArgument];
+	while (isspace(arg.front())) {
+		arg.erase(0, 1);
+	}
+	while (isspace(arg.back())) {
+		arg.erase(arg.length() - 1, 1);
+	}
+	return arg;
+}
+
+bool checkFileFormat(std::string& str, std::string expectedFormat) {
+	std::string format;
+
+	std::string::iterator pos = find(str.begin(), str.end(), '.');
+	if (pos == str.end()) {
+		return 1;
+	}
+	++pos;
+	while (pos != str.end()) {
+		format += (*pos);
+		++pos;
+	}
+	if (format == expectedFormat) {
+		return 0;
+	}
+	else {
+		return 1;
+	}
+}
+
+int getCmdArgs(int argc, char** argv, std::string& configFile, std::string& outputFile, std::vector<std::string>& inputFiles, bool& help) {
+	int indexArgument = 1;
+	bool output = false;
+
+	while (indexArgument < argc) {
+		//std::string argument = argv[currentArgument];
+		std::string currentArgument = getCmdArgument(argv, indexArgument);
+		++indexArgument;
+
+		if (currentArgument == "-h") {
+			help = true;
+		}
+		else if (currentArgument == "-c") {
+			if (indexArgument == argc) {
+				return 1; //throw exception
+			}
+			configFile = getCmdArgument(argv, indexArgument);
+			++indexArgument;
+			if (checkFileFormat(configFile, "txt") == 1) {
+				return 1;
+			}
+		}
+		else {
+			//std::string inputFile = getCmdArgument(argv, currentArgument);
+			if (checkFileFormat(currentArgument, "wav") == 1) {
+				return 1; //throw exception
+			}
+			if (output == false) {
+				outputFile = std::move(currentArgument);
+				output = true;
+			}
+			else {
+				inputFiles.push_back(std::move(currentArgument));
+			}
+		}
+	}
+	return 0;
+}
+
+int main(int argc, char** argv) {
+
+	std::string outputFile;
+	std::vector<std::string> inputFiles;
+	std::string configFile;
+	bool help = false;
+
+	if (getCmdArgs(argc, argv, configFile, outputFile, inputFiles, help) == 1) {
+		std::cout << "BAD" << std::endl;
+		return -1;
+	}
 
 	//std::string inputFile = "Ring08.wav";
-	std::string inputFile = "funkorama.wav";
+	//std::string inputFile = "funkorama.wav";
 
 
-	std::string configFile = "config.txt";
+	//std::string configFile = "config.txt";
 	std::vector<std::vector<std::string> > config;
 	readConfig readConfig(configFile);
 	readConfig.read(config);
 
-	Thread thread(std::make_shared<std::string>(inputFile));
+	Thread thread(std::make_shared<std::string>(inputFiles[0]));
 
 	for (size_t i = 0; i < config.size(); ++i) {
 
@@ -49,7 +129,7 @@ int main() {
 		}
 
 		std::vector<unsigned int> time_args;
-		for (int j = threads_number+1; j < config[i].size(); ++j) { // miss name of converter and input threads
+		for (size_t j = threads_number+1; j < config[i].size(); ++j) { // miss name of converter and input threads
 			time_args.push_back(stoi(config[i][j]));
 		}
 
